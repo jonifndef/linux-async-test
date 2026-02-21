b1dec7d641ae3f9bb4698aef103935caf5596360
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2026-02-21 14:39:04 +0100

    Minor refactor of minor_ui_uring_test.c

--- minor_io_uring_test.c
+++ minor_io_uring_test.c
@@ -1,4 +1,3 @@
-// minimal_uring_server.c: tiny io_uring-based HTTP server using accept(), recv(), write()
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,7 +7,11 @@
 
 int main(void) {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in addr = { .sin_family=AF_INET, .sin_addr.s_addr=htonl(INADDR_ANY), .sin_port=htons(8080) };
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_addr.s_addr = htonl(INADDR_ANY),
+        .sin_port = htons(8080)
+    };
 
     if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
       perror("bind");
@@ -17,7 +20,8 @@ int main(void) {
 
     listen(server_fd, 128);
 
-    struct io_uring ring; io_uring_queue_init(64, &ring, 0);
+    struct io_uring ring;
+    io_uring_queue_init(64, &ring, 0);
 
     const char resp[] =
         "HTTP/1.1 200 OK\r\n"
@@ -32,8 +36,13 @@ int main(void) {
         struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
         io_uring_prep_accept(sqe, server_fd, (struct sockaddr *)&cli, &clilen, 0);
         io_uring_submit(&ring);
-        struct io_uring_cqe *cqe; io_uring_wait_cqe(&ring, &cqe);
-        int c = cqe->res; io_uring_cqe_seen(&ring, cqe);
+
+        struct io_uring_cqe *cqe;
+        io_uring_wait_cqe(&ring, &cqe);
+        int c = cqe->res;
+
+        io_uring_cqe_seen(&ring, cqe);
+
         if (c < 0) continue;
 
         char buf[1024];
